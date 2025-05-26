35ce3d9bd34f1a50fffc3929e5737fc4650e8a2a
oculusK <kay5990@naver.com>
2025-05-26 10:27:52 +0900

    feature 공통 로그 모듈 사용

---
+++ src/main/java/com/nhnacademy/iot_service/config/FeignClientConfig.java
@@ -0,0 +1,15 @@
+package com.nhnacademy.iot_service.config;
+
+import com.nhnacademy.traceloggermodule.config.FeignTraceInterceptor;
+import feign.RequestInterceptor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class FeignClientConfig {
+
+    @Bean
+    public RequestInterceptor feignTraceInterceptor() {
+        return new FeignTraceInterceptor();
+    }
+}
