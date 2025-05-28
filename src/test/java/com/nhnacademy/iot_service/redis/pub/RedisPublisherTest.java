8218bd594b54756e58667d7feb35cb40d25b4be6
oculusK <kay5990@naver.com>
2025-05-28 14:54:13 +0900

    feature redis config update

--- src/test/java/com/nhnacademy/iot_service/redis/pub/RedisPublisherTest.java
+++ src/test/java/com/nhnacademy/iot_service/redis/pub/RedisPublisherTest.java
@@ -1,5 +1,8 @@
 package com.nhnacademy.iot_service.redis.pub;
 
+import com.nhnacademy.iot_service.dto.action.ActionResult;
+import com.nhnacademy.iot_service.dto.condition.ConditionResult;
+import com.nhnacademy.iot_service.dto.engine.RuleEvaluationResult;
 import com.nhnacademy.iot_service.dto.sensor.SensorResult;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
@@ -11,6 +14,7 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.listener.ChannelTopic;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 import static org.mockito.Mockito.*;
@@ -28,7 +32,21 @@ class RedisPublisherTest {
     RedisPublisher redisPublisher;
 
     private final String testTopicName = "sensor-service";
-    private final SensorResult testResult = new SensorResult("sensor1", "room1", "ON", List.of());
+    private final SensorResult testResult = new SensorResult("sensor1", "room1", "ON", List.of(
+            new RuleEvaluationResult(
+                    101L,
+                    "온도 과열 감지",
+                    true,
+                    List.of(
+                            new ConditionResult(1L, "temperature", "GT", "30", true)
+                    ),
+                    List.of(
+                            new ActionResult(1L, true, "EMAIL", "이메일 발송 성공", "mail-123", LocalDateTime.now())
+                    ),
+                    "온도 30도 초과, 이메일 발송",
+                    LocalDateTime.now()
+            )
+    ));
 
     @BeforeEach
     void setUp() {
